9729f1052637030747d3080b2d886af9f373687e
s1741640 <l.h.hamelers@student.utwente.nl>
2016-12-21 13:57:23 +0100

    Validmove verplaatsy

--- src/model/Humanplayer.java
+++ src/model/Humanplayer.java
@@ -6,26 +6,63 @@ public class Humanplayer extends Player {
 // x = column, y = height, z = row;
 	private Scanner in;
 
-	public Humanplayer(String name, control.Game game) {
-		super(name, game);
+	public Humanplayer(String name, Color color) {
+		super(name, color);
 		in = new Scanner(System.in);
 	}
 
 	@Override
-	public void determineMove(Board board) {
+	public Field determineMove(Board board) {
+		boolean valid = false;
+		Field end = new Field(0, 0, 0, null);
+		Field choice = new Field(0, 0, 0, color);
 		int col = askColumn();
 		int row = askRow();
 		int height = askHeight();
 		Color tile = this.getColor();
 		Field place = new Field(col, height, row, tile);
-		if (game.validMove(place, board)) {
-			makeMove(board, place);
+		if (board.boardEmpty() && height == 0) {
+			valid = true;
+		}
+		if (board.isEmpty(col, row, height) && height == 0) {
+			valid = true;
+		}
+		if (height >= 4) {
+			valid = false;
+		}
+		if (board.isEmpty(col, row, height) && height != 0) {
+			if (board.isEmpty(col, row - 1, height)) {
+				valid = false;
+			} else {
+				valid = true;
+			}
+		}
+		if(valid == false){
+			System.out.println("That is not a valid place, try again!");
+		}
+		else{
+			end = choice;
+		}
+		return end;
+
+	}
+	
+		
+		
+		
+		
+		
+		
+/*		if (game.validMove(place, board)) {
+			choice = place;
 		} else {
 			System.out.println("This is not a valid move. Try again.");
 		}
+		return choice;
+		
 
 
-	}
+	} */
 
 	public int askColumn() {
 		System.out.println("In which column would you like to place your tile?");
