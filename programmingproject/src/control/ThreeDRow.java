bd7bd9fa291b15fc553e8e3d8f34a3e4e158d391
altenburga <a.altenburg@student.utwente.nl>
2017-01-27 15:14:27 +0100

    ThreeDRow

--- programmingproject/src/control/ThreeDRow.java
+++ programmingproject/src/control/ThreeDRow.java
@@ -1,13 +1,35 @@
 package control;
 
-import model.*;
+import java.util.Scanner;
+
+import model.Color;
+import model.Computerplayer;
+import model.Humanplayer;
+import model.Player;
 
 public class ThreeDRow {
+	
 	public static void main(String[] args) {
-		Player p = new Computerplayer("Lieke");
-		Player q = new Computerplayer("Amber");
+		Player p = determinePlayer(args[0], Color.RED);
+		Player q = determinePlayer(args[1], Color.YEL);
 		Game battle = new Game(p, q);
 		
 		battle.start();
+	}
+	
+	public static Player determinePlayer(String arg, Color color) {
+		Player player = null;
+		String argument = arg.toUpperCase();
+		Scanner scanner = new Scanner(System.in);
+		if (argument.equals("-H")) {
+			System.out.println("What is your name?");
+			String name = scanner.nextLine();
+			player = new Humanplayer(name);
+		} else if (argument.equals("-C")) {
+			System.out.println("What is the name of the computer player?");
+			String compName = scanner.nextLine();
+			player = new Computerplayer(compName);
+		}
+		return player;
 	}
 }
